f38d3f2204218d8375afffcb3b065483bc73c212
0testarossa <lumina.fatetestarossa@gmail.com>
2021-01-07 18:18:28 +0100

    build fix2

---
+++ client/build/precache-manifest.01ac1b09df19bb7748029dae0564067b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991e06e98263624c5b4e2731379a2372",
+    "url": "./index.html"
+  },
+  {
+    "revision": "257d418574c4b682dcf2",
+    "url": "./static/css/main.1c4f54c1.chunk.css"
+  },
+  {
+    "revision": "8c4751da78a459f34bc9",
+    "url": "./static/js/2.36727acf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.36727acf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257d418574c4b682dcf2",
+    "url": "./static/js/main.c7725e68.chunk.js"
+  },
+  {
+    "revision": "384eef6b488672ae4321",
+    "url": "./static/js/runtime-main.3a633a5c.js"
+  }
+]);
