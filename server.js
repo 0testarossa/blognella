49c65f174c371525267b25ad9996783ee700d02c
0testarossa <lumina.fatetestarossa@gmail.com>
2021-01-07 20:42:51 +0100

    setup proxy

--- server.js
+++ server.js
@@ -54,9 +54,3 @@ mongoose
     throw error
   })
 
-const proxy = require('http-proxy-middleware')
-
-module.exports = function(app) {
-    // add other server routes to path array
-    app.use(proxy([''], { target: 'http://localhost:4000' }));
-} 
